fc886ffa6d1844c723916cba037d14905812e4ba
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-08-19 07:33:18 -0300

    removing old heroku api

--- frontend/src/data/services/api.ts
+++ frontend/src/data/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'http://localhost:3333/v1'
-  baseURL: 'https://api-esoja.herokuapp.com/v1'
+  baseURL: 'http://localhost:3333/v1'
 });
