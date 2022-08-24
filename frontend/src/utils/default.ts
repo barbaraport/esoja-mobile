1033369bcd7eea329b959282ca8da32fcad943a0
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-08-24 07:17:58 -0300

    updating firebase data

--- frontend/src/utils/default.ts
+++ frontend/src/utils/default.ts
@@ -1,5 +1,5 @@
 export const defaultUserImage =
-  'X';
+  'https://firebasestorage.googleapis.com/v0/b/esoja-90347.appspot.com/default.jpg?alt=media&token=d4bdb1cb-97c6-4db9-b669-725b07813588';
 
 export const defaultImage =
-  'X';
+  'https://firebasestorage.googleapis.com/v0/b/esoja-90347.appspot.com/SD-default-image.png?alt=media&token=91791b82-b292-4222-98a3-8c8ad1f77412';
