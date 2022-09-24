6617db74781a5c140e5a8b64fc24028e368442e6
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-09-24 20:18:21 -0300

    fixing bug inside the strong text styled component

--- frontend/src/components/StrongText/index.tsx
+++ frontend/src/components/StrongText/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StrongTextStyled } from './styles';
 
 interface StrongTextProps {}
+
 export const StrongText: React.FC<StrongTextProps> = ( children ) => {
-  return <StrongTextStyled>{children}</StrongTextStyled>;
+  return <StrongTextStyled>{Object.values(children)[0]}</StrongTextStyled>;
 };
